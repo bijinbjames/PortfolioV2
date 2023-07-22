f3551bafe9be052428a9304bda5589a4215371c8
Bijin B James <53501933+bijinbjames@users.noreply.github.com>
2023-07-22 22:56:07 +0530

    Hero page done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,17 +19,19 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>BIJIN B JAMES</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+          Unleashing the full potential of the web through code.<br className='sm:block hidden' />
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
+      <div>
+        <br></br>
+      </div>
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-40 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+          <div className='w-[35px] h-[64px] xs:w-[35px] xs:h-[64px] md:w-[25px] md:h-[53px] rounded-3xl border-4 border-secondary flex justify-center items-start p-1'>
             <motion.div
               animate={{
                 y: [0, 24, 0],
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,108 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-const Computers = () => {
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/laptopconcept.gltf");
+
+  // Set initial rotation angles
+  const [rotationY, setRotationY] = useState(0);
+
+  // Use the useFrame hook to update the rotation on each frame
+  useFrame(({ isScrolling }) => {
+    if (!isScrolling) {
+      setRotationY((rotationY) => rotationY + 0.005); // Adjust the speed by changing the increment value
+    }
+  });
+
   return (
-    <div>Computers</div>
-  )
-}
+    <mesh rotation={[0, rotationY, 0]}>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={2}
+        penumbra={1}
+        intensity={7}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <pointLight intensity={0} />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? 2.3 : 2.75}
+        position={isMobile ? [0, -2, 0] : [0, -3.25, -1.5]}
+        rotation={[0.4, -0, -0]}
+      />
+    </mesh>
+  );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const isScrollingRef = useRef(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      isScrollingRef.current = true;
+
+      // Clear the isScrollingRef after a short delay
+      clearTimeout(isScrollingRef.current);
+      isScrollingRef.current = setTimeout(() => {
+        isScrollingRef.current = false;
+      }, 100);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          enablePan={!isMobile} // Disable panning on mobile devices
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default ComputersCanvas;
